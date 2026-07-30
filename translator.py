1d5d62b448231a8163ce7bd76177a704598078dd
yousuf-sarfraz <yousufsarfraz27@gmail.com>
2026-07-30 13:49:54 +0500

    update translator.py

--- translator.py
+++ translator.py
@@ -1,1 +1,18 @@
-c
+from deep_translator import GoogleTranslator
+
+
+class LanguageTranslator:
+
+    def __init__(self):
+        self.languages = GoogleTranslator().get_supported_languages(as_dict=True)
+
+    def get_languages(self):
+        return self.languages
+
+    def translate_text(self, text, source, target):
+        translator = GoogleTranslator(
+            source=source,
+            target=target
+        )
+
+        return translator.translate(text)
